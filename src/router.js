33575832531044e9d1e25b3352a0c6da273ee56b
Achsuthan <achsuthan@icloud.com>
2020-05-02 21:44:50 +0530

    1. router validation added

--- src/router.js
+++ src/router.js
@@ -9,19 +9,38 @@ let router = new Router({
         {
             path: '/login',
             name: "Login",
-            component: () => import('./Views/Login')
+            component: () => import('./Views/Login'),
+            meta:{
+                isAuth: false
+            }
         },
         {
             path: '/home',
             name: "Home",
-            component: () => import('./Views/Home')
+            component: () => import('./Views/Home'),
+            meta:{
+                isAuth: true
+            }
         },
         {
             path: '*',
             name: "Login2",
-            component: () => import('./Views/Login')
+            component: () => import('./Views/Login'),
+            meta:{
+                isAuth: false
+            }
         }
     ]
+})
+
+router.beforeEach((to,from,next)=>{
+    if(to.matched.some(record => record.meta.isAuth)){
+        let user = JSON.parse(localStorage.getItem('user'))
+        if(!user){
+            next('/login')
+        }
+    }
+    next()
 })
 
 export default router
